b4187c086e84f3076cd713f3df901574c71ccd59
scjh <shichenjunhao@wps.cn>
2025-07-08 09:57:12 +0800

    Initial commit

---
+++ mathutil.go
@@ -0,0 +1,12 @@
+// mathutil/mathutil.go
+package mathutil
+
+// Add 计算两个整数的和
+func Add(a int, b int) int {
+	return a + b
+}
+
+// Multiply 计算两个整数的积
+func Multiply(a int, b int) int {
+	return a * b
+}
